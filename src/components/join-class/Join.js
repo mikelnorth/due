4767c27f6f9f27b97362c15c7679e414277ac484
keithdidier <keith.b.didier@gmail.com>
2017-11-17 23:59:35 -0700

    Added modal and mobile styling

--- src/components/join-class/Join.js
+++ src/components/join-class/Join.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
-import axios from "axios"
-import './Join.css'
+import axios from "axios";
+import './Join.css';
+import { Button, Header, Image, Modal } from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
+import MediaQuery from 'react-responsive';
+
 
 class Join extends Component {
     constructor(props) {
@@ -37,16 +41,48 @@ class Join extends Component {
     render() {
         console.log(this.state.classes)
         return (
-            <div>
+            <div className="classes-container">
+                <MediaQuery query="(min-width: 1024.1px)">
+                <Modal open={true}>
+                    <Modal.Header>Select a Teacher</Modal.Header>
+                    <Modal.Description>
+                    
+                        <div className="join-class-column"> 
+                    <span>Teacher</span><span>Days of Week</span>
+                </div>
                 {this.state.classes.map((clss, index) => {
                     return (
                         <button className='classes' onClick={() => this.selectCal(clss.calendar_id)}>
-                            {clss.calendar_name}
-                            {clss.days}
+                            <div className="teacher-and-days">
+                                <span>{clss.calendar_name}</span>
+                                <span>{clss.days}</span>
+                            </div>
                         </button>
                     )
                 })}
-                <button onClick={this.submitCalendar}>Submit</button>
+                <Link to="/dashboard" className="submit-btn"><button  onClick={this.submitCalendar}>Submit</button></Link>
+                       
+                    </Modal.Description>
+                </Modal>
+                </MediaQuery>
+
+                <MediaQuery query="(max-width: 1024px)">                
+                <div className="join-class-column"> 
+                    <span>Teacher</span><span>Days of Week</span>
+                </div>
+                {this.state.classes.map((clss, index) => {
+                    return (
+                        <button className='classes' onClick={() => this.selectCal(clss.calendar_id)}>
+                            <div className="teacher-and-days">
+                                <span>{clss.calendar_name}</span>
+                                <span>{clss.days}</span>
+                            </div>
+                        </button>
+                    )
+                })}
+                <Link to="/dashboard" className="submit-btn"><button  onClick={this.submitCalendar}>Submit</button></Link>
+                </MediaQuery>
+                
             </div>
         )
     }

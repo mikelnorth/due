ba86c9d4b2f60db0506374e2af36ba11d1faeded
Mikel North <mikel.north@gmail.com>
2017-11-27 14:47:41 -0700

    class calendar, mobile nav update

--- db/assignments_get_class.sql
+++ db/assignments_get_class.sql
@@ -1,7 +1,8 @@
-SELECT cal.calendar_name, c.class_name, c.class_id FROM users as u
+SELECT a.assignment_name as title, a.datetime as start, a.datetime as end, c.class_name as desc, a.points_possible FROM users as u
 JOIN user_schools as us ON u.user_id = us.user_id
 JOIN schools as s ON s.school_id = us.school_id
 JOIN classes as c ON c.school_id = us.school_id
 JOIN calendar as cal ON cal.class_id = c.class_id
 JOIN user_calendars as uc ON uc.calendar_id = cal.calendar_id
-WHERE u.user_id = $1 AND uc.user_id = $1;
+JOIN assignments as a ON a.calendar_id = cal.calendar_id
+WHERE uc.user_id = $1 and u.user_id = $1 and c.class_id = $2;

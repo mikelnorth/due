93fa72980de15d93f8aa20f33c31afc4432c83f1
keithdidier <keith.b.didier@gmail.com>
2017-11-28 16:12:57 -0700

    colors added to calendar

--- server/server.js
+++ server/server.js
@@ -110,7 +110,7 @@ app.get('/api/classes/getbyclassname/:user_id', classes_controller.getClassNames
 
 //CALENDARS
 app.get('/api/calendars/get/:class_id', calendar_controller.findCalendars);
-app.post('/api/calendars/add/:user_id/:class_id', calendar_controller.createCalendar);
+app.post('/api/calendars/add/:user_id/:class_id/:color', calendar_controller.createCalendar);
 app.get('/api/calendars/user/:user_id', calendar_controller.findCalendarByUser);
 app.get('/api/calendars/user/class/:user_id/:calendar_id', calendar_controller.findCalendarByUserClass);
 

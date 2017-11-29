582c659b65db10ca60f43c2a106b723399f03962
Mikel North <mikel.north@gmail.com>
2017-11-28 19:00:17 -0700

    minor bug fixes

--- server/server.js
+++ server/server.js
@@ -19,78 +19,78 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(session({
-secret: process.env.SECRET,
-resave: false,
-saveUninitialized: false
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
 }))
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 massive(process.env.CONNECTION_STRING).then(db => {
-console.log("Connected to DB")
-app.set('db', db)
+    console.log("Connected to DB")
+    app.set('db', db)
 })
 
 passport.use(new Auth0Strategy({
-domain: process.env.AUTH_DOMAIN,
-clientID: process.env.AUTH_CLIENT_ID,
-clientSecret: process.env.AUTH_CLIENT_SECRET,
-callbackURL: process.env.CALLBACK_URL
+    domain: process.env.AUTH_DOMAIN,
+    clientID: process.env.AUTH_CLIENT_ID,
+    clientSecret: process.env.AUTH_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
 },
-function (accessToken, refreshToken, extraParams, profile, done) {
-//db calls will be used here, and create users in our database
+    function (accessToken, refreshToken, extraParams, profile, done) {
+        //db calls will be used here, and create users in our database
 
-const db = app.get('db')
-//find and add users here
+        const db = app.get('db')
+        //find and add users here
 
-console.log(profile)
-db.users_find_user([profile._json.email]).then(user => {
-if (user[0]){
-return done(null, user[0].user_id)
-}
-else {
-db.users_create_user([profile._json.name, profile._json.email, profile._json.picture])
-.then( user => {
-return done(null, user[0].user_id)
-})
-}
-console.log(user)
-})
+        console.log(profile)
+        db.users_find_user([profile._json.email]).then(user => {
+            if (user[0]) {
+                return done(null, user[0].user_id)
+            }
+            else {
+                db.users_create_user([profile._json.name, profile._json.email, profile._json.picture])
+                    .then(user => {
+                        return done(null, user[0].user_id)
+                    })
+            }
+            console.log(user)
+        })
 
-}
+    }
 ))
 
 app.get('/auth', passport.authenticate('auth0'));
 app.get('/auth/callback', passport.authenticate('auth0', {
-successRedirect: process.env.SUCCESS_REDIRECT,
-failureRedirect: process.env.FAILURE_REDIRECT
+    successRedirect: process.env.SUCCESS_REDIRECT,
+    failureRedirect: process.env.FAILURE_REDIRECT
 }))
 
-app.get("/auth/me", (req,res) => {
-if(!req.user){
-return res.status(404).send(false)
-}
+app.get("/auth/me", (req, res) => {
+    if (!req.user) {
+        return res.status(404).send(false)
+    }
 
-return res.status(200).send(req.user)
+    return res.status(200).send(req.user)
 })
 
 app.get("/auth/logout", (req, res) => {
-req.logout()
-res.redirect(302, "http://localhost:3000/#/")
+    req.logout()
+    res.redirect(302, "http://localhost:3000/#/")
 })
 
 passport.serializeUser(function (user_id, done) {
-done(null, user_id)
+    done(null, user_id)
 })
 
-passport.deserializeUser( function (user_id, done) {
-//deserialize here
-app.get('db').users_find_current_user([user_id])
-.then( user => {
-done(null, user[0])
-})
-// done(null, user_id)
+passport.deserializeUser(function (user_id, done) {
+    //deserialize here
+    app.get('db').users_find_current_user([user_id])
+        .then(user => {
+            done(null, user[0])
+        })
+    // done(null, user_id)
 })
 
 
@@ -133,4 +133,4 @@ app.get('/api/assignments/get/topfiveclass/:user_id/:cal_id', assignment_control
 
 const PORT = 3005;
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`)
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -8,82 +8,82 @@ import { Button, Header, Image, Modal } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css';
 
 class Profile extends Component {
-constructor(props) {
-super(props)
+    constructor(props) {
+        super(props)
 
-this.state = {
-open: false
-}
-}
-
-
-//deletes a class associated to the user.
-deleteClass(classId) {
-this.props.deleteClass(this.props.user.user_id, classId)
-}
-
-addAssignments(classId) {
-this.setState({
-open: !this.state.open
-})
-}
-
-render() {
-console.log(this.props.classInfo)
-return (
-<div className='profile'>
-<SideNav />
-<div className='profile-content'>
-<span>hello world</span>
-{this.props.classInfo.length !== 0 ?
-this.props.classInfo.map((clss, index) => {
-return (
-//returns a button for every class with access to the name, subject, and id
-<div className='edit-class' key={clss.calendar_id}>
-<span>{clss.class_name}</span>
-<span className='delete' onClick={() => this.deleteClass(clss.calendar_id)}>x</span>
-</div>
-)
-})
-: null}
-
-<span>Admin calendars</span>
-{console.log(this.props.adminCalendars)}
-{this.props.adminCalendars.length !== 0 ?
-this.props.adminCalendars.map((clss, index) => {
-return (
-//returns a button for every class with access to the name, subject, and id
-<div className='edit-class' key={clss.class_id}>
-<span>{clss.calendar_name}</span>
-<span className='Add' onClick={() => this.addAssignments(clss.class_id)}>+</span>
-</div>
-)
-})
-: null}
+        this.state = {
+            open: false
+        }
+    }
 
 
+    //deletes a class associated to the user.
+    deleteClass(classId) {
+        this.props.deleteClass(this.props.user.user_id, classId)
+    }
 
-</div>
+    addAssignments(classId) {
+        this.setState({
+            open: !this.state.open
+        })
+    }
+
+    render() {
+        console.log(this.props.classInfo)
+        return (
+            <div className='profile'>
+                <SideNav />
+                <div className='profile-content'>
+                    <span>hello world</span>
+                    {this.props.classInfo.length !== 0 ?
+                        this.props.classInfo.map((clss, index) => {
+                            return (
+                                //returns a button for every class with access to the name, subject, and id
+                                <div className='edit-class' key={clss.calendar_id}>
+                                    <span>{clss.class_name}</span>
+                                    <span className='delete' onClick={() => this.deleteClass(clss.calendar_id)}>x</span>
+                                </div>
+                            )
+                        })
+                        : null}
+
+                    <span>Admin calendars</span>
+                    {console.log(this.props.adminCalendars)}
+                    {this.props.adminCalendars.length !== 0 ?
+                        this.props.adminCalendars.map((clss, index) => {
+                            return (
+                                //returns a button for every class with access to the name, subject, and id
+                                <div className='edit-class' key={clss.class_id}>
+                                    <span>{clss.calendar_name}</span>
+                                    <span className='Add' onClick={() => this.addAssignments(clss.class_id)}>+</span>
+                                </div>
+                            )
+                        })
+                        : null}
 
 
-<Modal open={this.state.open}>
-<span className='delete' onClick={() => this.addAssignments()}>X</span>
-<Modal.Header>Join or Create a Class</Modal.Header>
-<Modal.Content image style={{ padding: '0px' }}>
-<p>adding class</p>
-</Modal.Content>
-</Modal>
-</div>
-)
-}
+
+                </div>
+
+
+                <Modal open={this.state.open}>
+                    <span className='delete' onClick={() => this.addAssignments()}>X</span>
+                    <Modal.Header>Join or Create a Class</Modal.Header>
+                    <Modal.Content image style={{ padding: '0px' }}>
+                        <p>adding class</p>
+                    </Modal.Content>
+                </Modal>
+            </div>
+        )
+    }
 }
 
 //used redux to make the user object acessible.
 function mapStateToProps(state) {
-return {
-user: state.user,
-classInfo: state.classInfo,
-adminCalendars: state.adminCalendars
-}
+    return {
+        user: state.user,
+        classInfo: state.classInfo,
+        adminCalendars: state.adminCalendars
+    }
 }
 export default connect(mapStateToProps, { getUser, deleteClass })(Profile);

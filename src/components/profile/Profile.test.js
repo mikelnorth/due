85774220915d03a99802e93caf6c09d759e36caa
Mikel North <mikel.north@gmail.com>
2017-12-05 14:35:48 -0700

    test

---
+++ src/components/profile/Profile.test.js
@@ -0,0 +1,12 @@
+// test('get users', () => {
+//     expect.assertions(1);
+//     const url = "http://localhost:3005/api/users/setuser/36"
+//     return axios.get(url).then(res => {
+//         console.log(res)
+//         expect(res).toEqual('')
+//     })
+// })
+
+test('test', () => {
+    expect(2).toEqual(2)
+})

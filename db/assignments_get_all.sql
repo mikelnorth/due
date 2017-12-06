5341a5928734fb41a9cfc354cc4daf203824a34c
Travis Bates <travbates93@gmail.com>
2017-12-05 22:34:47 -0700

    Fixed duplicate assignments bug

--- db/assignments_get_all.sql
+++ db/assignments_get_all.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT a.assignment_id, a.assignment_name as title, a.datetime as start, a.datetime as end, c.class_name as desc, a.points_possible, uc.color, ua.completed FROM users as u
+SELECT DISTINCT a.assignment_id, a.assignment_name as title, a.datetime as start, a.datetime as end, c.class_name as desc, a.points_possible, uc.color, ua.completed, a.category FROM users as u
 JOIN user_schools as us ON u.user_id = us.user_id
 JOIN schools as s ON s.school_id = us.school_id
 JOIN classes as c ON c.school_id = us.school_id
@@ -6,4 +6,4 @@ JOIN calendar as cal ON cal.class_id = c.class_id
 JOIN user_calendars as uc ON uc.calendar_id = cal.calendar_id
 JOIN assignments as a ON a.calendar_id = cal.calendar_id
 JOIN user_assignment as ua ON ua.assignment_id = a.assignment_id
-WHERE uc.user_id = $1 and u.user_id = $1;
+WHERE u.user_id = $1 and ua.user_id = $1 and us.user_id = $1 and uc.user_id = $1;
--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -3,12 +3,16 @@ import React, { Component } from 'react';
 import Headroom from "react-headroom"
 import scrollIntoViewIfNeeded from 'scroll-into-view-if-needed'
 import "../../../node_modules/animate.css/animate.min.css"
-//import ScrollAnimation from 'react-animate-on-scroll';
+import ScrollAnimation from 'react-animate-on-scroll';
+import "animate.css/animate.min.css";
+
 
 import "./Landing.css"
+import "./Devices.css"
+
 import logo from "../../assets/due-logo.png"
-import lappy from "../../assets/Stuff.png"
-import iphone from "../../assets/iPhone X.png"
+import lappy from "../../assets/MBP@4x.png"
+import iphone from "../../assets/iPhone@4x.png"
 
 
 
@@ -36,7 +40,7 @@ class Landing extends Component {
 
   setHowNode = (node) => {
     if (node) {
-      this._howNode = node
+      this._HowNode = node
     }
   }
 
@@ -64,7 +68,7 @@ class Landing extends Component {
     event.preventDefault()
 
     // Passing the dom node from react is all you need for this to work
-    scrollIntoViewIfNeeded(this._aboutNode, {
+    scrollIntoViewIfNeeded(this._HowNode, {
       duration: 500,
       easing: 'easeIn'
     })
@@ -75,45 +79,60 @@ class Landing extends Component {
       <div className="Landing" >
         <div ref={this.setTopNode}></div>
 
-        <Headroom
-          
-          style={{
-            height: 60,
-            background: 'rgba(0, 0, 255, 1)',
-            boxShadow: '1px 1px 1px rgba(0,0,0,0.25)',
-          }}
-        >
-          <header>
-            <div className="sideBySide" onClick={this.goToTop}>
-              <img src={logo} className="logo" alt="logo"></img>
-              <span className="dueTitle">due.</span>
+        <header className="header">
+          <div className="sideBySideL" onClick={this.goToTop}>
+            <img src={logo} className="logo" alt="logo"></img>
+            <span className="dueTitle">due.</span>
+          </div>
+          <div className="sideBySideL">
+            <span className="links" onClick={this.goToAbout}>About</span>
+            <div className="divider" />
+            <span className="links" onClick={this.goToHow}>How it works</span>
+            <div className="divider" />
+
+            <span className="links" onClick={this.goToMore}>More info</span>
+
+            <a href={process.env.REACT_APP_LOGIN}><button className="login">Login</button></a>
+          </div>
+        </header>
+        <div className="placeholder">
+          {/* <ScrollAnimation animateIn="flipInX" animateOut="fadeOut"><img src={iphone} className="iphone" alt="iphone"></img></ScrollAnimation> */}
+          {/* <ScrollAnimation animateIn="flipInX" animateOut="fadeOut"><img src={lappy} className="lappy" alt="lappy"></img></ScrollAnimation> */}
+          <div style={{marginTop: '100px'}}>
+            <div class="marvel-device iphone-x">
+              <div class="notch">
+                <div class="camera"></div>
+                <div class="speaker"></div>
+              </div>
+              <div class="top-bar"></div>
+              <div class="sleep"></div>
+              <div class="bottom-bar"></div>
+              <div class="volume"></div>
+              <div class="overflow">
+                <div class="shadow shadow--tr"></div>
+                <div class="shadow shadow--tl"></div>
+                <div class="shadow shadow--br"></div>
+                <div class="shadow shadow--bl"></div>
+              </div>
+              <div class="inner-shadow"></div>
+              <div class="screen">
+                <img src={logo} style={{width: '275px', marginTop: '250px'}}/>
             </div>
-            <div className="sideBySide">
-              <span className="links" onClick={this.goToAbout}>About</span>
-              <span className="links" onClick={this.goToHow}>How it works</span>
-              <span className="links">More info</span>
-
-              <a href={process.env.REACT_APP_LOGIN}><button className="login">Login</button></a>
             </div>
-          </header>
-        </Headroom>
-        <div className="placeholder">
-          <img src={iphone} className="iphone" alt="iphone"></img>
-          <img src={lappy} className="lappy" alt="lappy"></img>
+          </div>
         </div>
         <div className="placeholder">
-        </div>
-
-        <div className="placeholder">
-          <div ref={this.setAboutNode}></div>
+          About
 
         </div>
-
+        <div ref={this.setAboutNode}></div>
         <div className="placeholder">
-
-
+          How It Works
         </div>
         <div ref={this.setHowNode}></div>
+        <div className="placeholder">
+          More Info
+        </div>
       </div>
     );
   }
